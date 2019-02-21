bdd13c672b82835f3634b8fa27a20dc2a41d1281
Lorenzo Evans <lorenzo.evans94@gmail.com>
2019-02-20 20:31:44 -0500

    Implementing mui components/style/theme, will merge branches if successful.

--- lambda-notes/src/components/NoteContainer/NoteContainer.js
+++ lambda-notes/src/components/NoteContainer/NoteContainer.js
@@ -3,7 +3,26 @@ import Note from './Note'
 import { connect } from 'react-redux'
 import { fetchNotes } from '../../actions/index'
 import { withRouter } from 'react-router'
-import { NoteContainerStyle } from './styled-components/NoteContainerStyledComponents'
+import classNames from 'classnames';
+import { withStyles } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Drawer from '@material-ui/core/Drawer';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import List from '@material-ui/core/List';
+import Typography from '@material-ui/core/Typography';
+import Divider from '@material-ui/core/Divider';
+import IconButton from '@material-ui/core/IconButton';
+import Badge from '@material-ui/core/Badge';
+import MenuIcon from '@material-ui/icons/Menu';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import NotificationsIcon from '@material-ui/icons/Notifications';
+// import { mainListItems, secondaryListItems } from './listItems';
+// import SimpleLineChart from './SimpleLineChart';
+// import SimpleTable from './SimpleTable';
+
+
+// import { NoteContainerStyle } from './styled-components/NoteContainerStyledComponents'
 
 class NoteContainer extends React.Component {
 
@@ -12,16 +31,19 @@ class NoteContainer extends React.Component {
  }
  render(){
   return (
-    <NoteContainerStyle className="note_container">
-     {this.props.notes.map((note, index) => 
-     <Note 
-      key={index}
-      title={note.title}
-      textBody={note.textBody}
-      id={note._id}
-     />
-     )}
-    </NoteContainerStyle>
+    // <NoteContainerStyle className="note_container">
+    //  {this.props.notes.map((note, index) => 
+    //  <Note 
+    //   key={index}
+    //   title={note.title}
+    //   textBody={note.textBody}
+    //   id={note._id}
+    //  />
+    //  )}
+    // </NoteContainerStyle>
+    <div>
+
+    </div>
   )
  }
 }
--- lambda-notes/src/components/Sidebar/Sidebar.js
+++ lambda-notes/src/components/Sidebar/Sidebar.js
@@ -1,22 +1,87 @@
 import React from 'react'
-import { Button } from 'reactstrap'
-import { Link } from 'react-router-dom'
-import { H1, Div, ContainerStyle } from './styled-components/SidebarStyledComponents'
+// import { Link } from 'react-router-dom'
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import ListSubheader from '@material-ui/core/ListSubheader';
+import DashboardIcon from '@material-ui/icons/Dashboard';
+import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
+import PeopleIcon from '@material-ui/icons/People';
+import BarChartIcon from '@material-ui/icons/BarChart';
+import LayersIcon from '@material-ui/icons/Layers';
+import AssignmentIcon from '@material-ui/icons/Assignment';
+import { Divider } from '@material-ui/core'
+
+// import { H1, Div, ContainerStyle } from './styled-components/SidebarStyledComponents'
 
 const Sidebar = props => {
  return (
-      <ContainerStyle>
-       <H1>Duly Noted</H1>
-       <Link to="/">
-       <Div>
-       <Button color="info">
+  <div>
+   <ListItem button>
+      <ListItemIcon>
+        <DashboardIcon />
+      </ListItemIcon>
+      <ListItemText primary="Dashboard" />
+    </ListItem>
+    <ListItem button>
+      <ListItemIcon>
+        <ShoppingCartIcon />
+      </ListItemIcon>
+      <ListItemText primary="Orders" />
+    </ListItem>
+    <ListItem button>
+      <ListItemIcon>
+        <PeopleIcon />
+      </ListItemIcon>
+      <ListItemText primary="Customers" />
+    </ListItem>
+    <ListItem button>
+      <ListItemIcon>
+        <BarChartIcon />
+      </ListItemIcon>
+      <ListItemText primary="Reports" />
+    </ListItem>
+    <ListItem button>
+      <ListItemIcon>
+        <LayersIcon />
+      </ListItemIcon>
+      <ListItemText primary="Integrations" />
+    </ListItem>
+    <Divider />
+    <ListSubheader inset>Saved reports</ListSubheader>
+    <ListItem button>
+      <ListItemIcon>
+        <AssignmentIcon />
+      </ListItemIcon>
+      <ListItemText primary="Current month" />
+    </ListItem>
+    <ListItem button>
+      <ListItemIcon>
+        <AssignmentIcon />
+      </ListItemIcon>
+      <ListItemText primary="Last quarter" />
+    </ListItem>
+    <ListItem button>
+      <ListItemIcon>
+        <AssignmentIcon />
+      </ListItemIcon>
+      <ListItemText primary="Year-end sale" />
+    </ListItem>
+  </div>
+  // <ContainerStyle>
+      //  <H1>Duly Noted</H1>
+      //  <Link to="/">
+      //  <Div>
+      //  <Button color="info">
        
-       View Your Notes.</Button></Div></Link>
-       {' '}
-       <Link to="/create"><Button color="info">
-       <Div>
-       Add Note +</Div></Button></Link>
-      </ContainerStyle>
+      //  View Your Notes.</Button></Div></Link>
+      //  {' '}
+      //  <Link to="/create"><Button color="info">
+      //  <Div>
+      //  Add Note +</Div></Button></Link>
+      // </ContainerStyle>
+
+   
     )
 }
 
